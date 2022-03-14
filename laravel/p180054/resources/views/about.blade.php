b937248c50cb521877aba06227e3dc15e29c8a72
iffishells <iffishells@gmail.com>
2022-03-14 19:07:38 +0500

    laravel views and control

---
+++ laravel/p180054/resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@section('title' , 'About us')
+
+@extends('layout')
+@section('content')
+<div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+        <h1>About us</h1>
+    </div>
+
+    <div class="mt-8 dark:bg-gray-800 overflow-hidden">
+        <p>This is about Page</p>         
+    </div>
+
+</div>
+@endsection
+
+
+
---
+++ laravel/posty/resources/views/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posty</title>
+
+    <link rel='stylesheet' href="{{ asset('css/app.css')}}">
+
+</head>
+    <body  class="bg-gray-200">
+
+        <nav class="p-6 bg-white flex justify-between">
+            
+
+            <ul class="flex items-center">
+                <li>
+                <a href=""class='p-3'>HOME</a> 
+                </li>
+
+                <li>
+                <a href="" class='p-3'>Dashboard</a> 
+                </li>
+        
+                <li>
+                <a href=""class='p-3'>POST</a> 
+                </li>
+            </ul>
+
+            <ul class="flex items-center">
+                <li>
+                    <a href=""class='p-3'>Iffishells</a> 
+                </li>
+
+                <li>
+                    <a href="" class='p-3'>login</a> 
+                </li>
+        
+                <li>
+                    <a href=""class='p-3'>Register</a> 
+                </li>
+
+                <li>
+                    <a href=""class='p-3'>Logout</a> 
+                </li>
+            </ul>
+        </nav>
+        
+    @yield('content')
+        
+    </body>
+</html>
